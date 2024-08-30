b9ef803efe14fc0467cb7204e3d9468be6387236
HiraiKyo <kyo.mikhael@gmail.com>
2024-08-30 09:38:02 +0900

    Testing SSG with Github Pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,97 +1,41 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-import nextConfig from "../next.config.mjs"; // 追加
-const BASE_PATH = nextConfig.basePath ? nextConfig.basePath : ""; // 追加
+import Link from "next/link";
+// For Github Pages
+import nextConfig from "../next.config.mjs";
+const BASE_PATH = nextConfig.basePath ? nextConfig.basePath : "";
 
-export default function Home() {
+export default () => {
   return (
     <main className={styles.main}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>CA技研ホーム</h1>
+      </header>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src={`${BASE_PATH}/vercel.svg`}
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <Image
+            src={`${BASE_PATH}/ca5.png`}
+            alt="CA-Giken Logo"
+            className={styles.vercelLogo}
+            width={100}
+            height={100}
+            priority
+          />
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <Link
+          href="/aboutus"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            会社情報
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </Link>
       </div>
     </main>
   );
