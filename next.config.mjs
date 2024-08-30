901dac4cf259405d46f03fd7afab95318fb99b67
HiraiKyo <kyo.mikhael@gmail.com>
2024-08-30 10:15:37 +0900

    Removed explicit mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,10 @@ const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
-  output: "export",
+  output: 'export',
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 };
 
 const withMDX = createMDX({
@@ -16,4 +16,6 @@ const withMDX = createMDX({
 })
 
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+const config = withMDX(nextConfig)
+
+export default config;
