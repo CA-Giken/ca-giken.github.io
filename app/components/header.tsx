aecf03000d20ecb42539670c4ec45d27e57731c1
HiraiKyo <kyo.mikhael@gmail.com>
2024-10-09 20:10:45 +0900

    ハンバーガーメニュー実装

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,23 +1,48 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 import styles from "./header.module.css";
 
 export const Header = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const toggleMenu = () => setIsMenuOpen((prev) => !prev);
+
 	return (
 		<header className={styles.header}>
+			<div className={styles.logo}>
+				<Link href="/">CA技研</Link>
+			</div>
 			<nav className={styles.nav}>
-				<div className={styles.logo}>
-					<Link href="/">CA技研</Link>
-				</div>
-				<button className={styles.menuButton} type="button">
-					Menu
+				<button
+					className={styles.menuButton}
+					type="button"
+					aria-expanded={false}
+					aria-controls="navMenu"
+					onClick={toggleMenu}
+				>
+					{isMenuOpen ? "閉じる" : "メニュー"}
 				</button>
-				<div className={styles.navLinks}>
-					<Link href="/">ホーム</Link>
-					<Link href="/products">製品情報</Link>
-					<Link href="/blogs">ニュース</Link>
-					<Link href="/aboutus">当社について</Link>
-					<Link href="/contactus">お問い合わせ</Link>
-				</div>
+				<ul
+					id="navMenu"
+					className={`${styles.navLinks} ${isMenuOpen ? styles.active : ""}`}
+				>
+					<li>
+						<Link href="/">ホーム</Link>
+					</li>
+					<li>
+						<Link href="/products">製品情報</Link>
+					</li>
+					<li>
+						<Link href="/blogs">ニュース</Link>
+					</li>
+					<li>
+						<Link href="/aboutus">当社について</Link>
+					</li>
+					<li>
+						<Link href="/contactus">お問い合わせ</Link>
+					</li>
+				</ul>
 			</nav>
 		</header>
 	);
